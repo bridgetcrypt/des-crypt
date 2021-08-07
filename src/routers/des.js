39524f641a5af8aa74e226b07f4fafb82db8698b
vortech-studio <vortechstudio@gmail.com>
2021-08-07 13:24:24 +0300

    Refactored

--- src/routers/des.js
+++ src/routers/des.js
@@ -42,12 +42,13 @@ router.get("/decrypt", cors(corsOptions), async (req, res) => {
   const cipherMessage = req.query.cipherMessage;
   const key = req.query.key;
   var originalMessage = decryptMessage(cipherMessage, key);
-  console.log(originalMessage);
 
   //error checking. If text is "", then encryption was unsuccessful
   if ((originalText = "")) {
     res.status(400).send("Could not decrypt message");
   } else {
+    console.log("Original Message");
+    console.log(originalMessage);
     res.status(200).send(originalMessage);
   }
 });
